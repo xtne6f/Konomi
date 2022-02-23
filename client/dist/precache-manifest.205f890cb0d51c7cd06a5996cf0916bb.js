cf143f7bf99d0e5f7e369fa2783315542c47735c
xtne6f <xt4ubq@gmail.com>
2022-02-23 19:52:53 +0900

    Update: [Client] ビルドを更新

---
+++ client/dist/precache-manifest.205f890cb0d51c7cd06a5996cf0916bb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e444c90ccba511566ff",
+    "url": "/assets/css/app.7b2ec17c.css"
+  },
+  {
+    "revision": "92b09bee7bbf97db3e0b",
+    "url": "/assets/css/chunk-vendors.56bb2c10.css"
+  },
+  {
+    "revision": "9cacdc876e2049988fcab540c21738d5",
+    "url": "/assets/fonts/materialdesignicons-webfont.9cacdc87.eot"
+  },
+  {
+    "revision": "9d243c168a4f1c2cb3cec74884344de7",
+    "url": "/assets/fonts/materialdesignicons-webfont.9d243c16.woff2"
+  },
+  {
+    "revision": "a0711490bcd581b647329230b3e915cf",
+    "url": "/assets/fonts/materialdesignicons-webfont.a0711490.woff"
+  },
+  {
+    "revision": "b62641afc9ab487008e996a5c5865e56",
+    "url": "/assets/fonts/materialdesignicons-webfont.b62641af.ttf"
+  },
+  {
+    "revision": "63abc49a99bd463af26e73cec607771d",
+    "url": "/assets/img/icon.svg"
+  },
+  {
+    "revision": "83079d38a7a118e1c80fe28d139991d8",
+    "url": "/assets/img/logo.svg"
+  },
+  {
+    "revision": "14d74db9eb062b39dc128daeba77cb63",
+    "url": "/assets/img/player-backgrounds/01.jpg"
+  },
+  {
+    "revision": "98e077363a5eec17da30acef5038f924",
+    "url": "/assets/img/player-backgrounds/02.jpg"
+  },
+  {
+    "revision": "e75e4fc34090286e347cebf12c74b1b8",
+    "url": "/assets/img/player-backgrounds/03.jpg"
+  },
+  {
+    "revision": "714dd3c050c09a16236f2424c548c83f",
+    "url": "/assets/img/player-backgrounds/04.jpg"
+  },
+  {
+    "revision": "717125c34121b326e8f90773565f59ca",
+    "url": "/assets/img/player-backgrounds/05.jpg"
+  },
+  {
+    "revision": "aa3b22785383baf67ad6d53fee94ed1c",
+    "url": "/assets/img/player-backgrounds/06.jpg"
+  },
+  {
+    "revision": "dc9937f7a374b99981cb0d6c9a642e56",
+    "url": "/assets/img/player-backgrounds/07.jpg"
+  },
+  {
+    "revision": "b6cedbf1da35814fbf784591380fde62",
+    "url": "/assets/img/player-backgrounds/08.jpg"
+  },
+  {
+    "revision": "e989450375d6954b37b066a1cec3ad35",
+    "url": "/assets/img/player-backgrounds/09.jpg"
+  },
+  {
+    "revision": "417128b6120078997139b44ee2c73dbd",
+    "url": "/assets/img/player-backgrounds/10.jpg"
+  },
+  {
+    "revision": "8c173e2d5980e09dc7b0e36e97b8f189",
+    "url": "/assets/img/player-backgrounds/11.jpg"
+  },
+  {
+    "revision": "97231a4813562229cc55d4516cb85350",
+    "url": "/assets/img/player-backgrounds/12.jpg"
+  },
+  {
+    "revision": "9e444c90ccba511566ff",
+    "url": "/assets/js/app.b7a05f78.js"
+  },
+  {
+    "revision": "92b09bee7bbf97db3e0b",
+    "url": "/assets/js/chunk-vendors.f0d10b4d.js"
+  },
+  {
+    "revision": "4085dc552d797407a3c1ed3ec8f9f590",
+    "url": "/css/noto-sans-jp-v36-open-sans-v27.css"
+  },
+  {
+    "revision": "62d4ed5649a6027e9ec58490f852b6bf",
+    "url": "/fonts/noto-sans-jp-readme.txt"
+  },
+  {
+    "revision": "d807c87b5d74abc9d24f495eaeeb2ec7",
+    "url": "/fonts/noto-sans-jp-v36-japanese-500.woff"
+  },
+  {
+    "revision": "d3bcee6a074d1e0ccebb922e69ab0289",
+    "url": "/fonts/noto-sans-jp-v36-japanese-500.woff2"
+  },
+  {
+    "revision": "2abc0999f243566acf7a139cbbb0eb06",
+    "url": "/fonts/noto-sans-jp-v36-japanese-700.woff"
+  },
+  {
+    "revision": "3af4202137c7086cf9d0d0451ed801b4",
+    "url": "/fonts/noto-sans-jp-v36-japanese-700.woff2"
+  },
+  {
+    "revision": "504a810bbd1e20feefae992548f11572",
+    "url": "/fonts/open-sans-readme.txt"
+  },
+  {
+    "revision": "73c35c138bb57f5694dae3baede8f147",
+    "url": "/fonts/open-sans-v27-latin-600.woff"
+  },
+  {
+    "revision": "1f577b061e6e464e2c949faee6518469",
+    "url": "/fonts/open-sans-v27-latin-600.woff2"
+  },
+  {
+    "revision": "3d1b06f2d8f36c86fa679ff135335fa3",
+    "url": "/fonts/open-sans-v27-latin-700.woff"
+  },
+  {
+    "revision": "875ba54801f7cf83ea70abf613fab665",
+    "url": "/fonts/open-sans-v27-latin-700.woff2"
+  },
+  {
+    "revision": "1090d5edcf36fc0205eb6a15865e6d75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c392c216bc185a4042f0f521213a352",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d98e9ec5fafad691048cbb603b5b0e71",
+    "url": "/robots.txt"
+  }
+]);
--- client/dist/service-worker.js
+++ client/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.416d1ade8fddf620e77205b62cedc7fc.js"
+  "/precache-manifest.205f890cb0d51c7cd06a5996cf0916bb.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "KonomiTV"});
